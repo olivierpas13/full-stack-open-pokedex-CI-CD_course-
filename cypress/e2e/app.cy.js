c41ec3e43c3d70c3ccd73e4c87dd3f14ebfb0a26
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-09-12 22:33:36 -0500

    Exercise 11.9 completed

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', () => {
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
