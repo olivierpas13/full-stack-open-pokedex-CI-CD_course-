035231135460832431fdb2cd5cd828f53aeab6f8
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-09-12 22:22:50 -0500

    quickfix

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   video: false,
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {},
     supportFile: false,
     pageLoadTimeout: 100000
